d01150de57ce47c961e900de451071eab4f4e0d4
softprops <d.tangren@gmail.com>
2016-03-18 08:26:02 -0400

    flexible authentication

--- examples/search.rs
+++ examples/search.rs
@@ -1,14 +1,17 @@
 extern crate env_logger;
 extern crate goji;
+extern crate hyper;
 
-use goji::{Jira, SearchOptions};
+use hyper::Client;
+use goji::{Credentials, Jira, SearchOptions};
 use std::env;
 
 fn main() {
     env_logger::init().unwrap();
     if let (Some(host), Some(user), Some(pass)) = (env::args().nth(1), env::args().nth(2), env::args().nth(3)) {
-        let jira = Jira::new(host, user, pass);
-        println!("{:?}", jira.search(
+        let client = Client::new();
+        let jira = Jira::new(host, Credentials::Basic(user, pass), &client);
+        println!("{:#?}", jira.search(
             &SearchOptions::builder()
                 .jql("assignee=doug")
                 .build()
--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub mod errors;
 pub use errors::Error;
 pub mod rep;
 pub use rep::SearchResults;
-use hyper::client::Client;
+use hyper::client::{Client, RequestBuilder};
 use hyper::method::Method;
 use hyper::header::{ContentType, Authorization, Basic};
 use std::io::Read;
@@ -21,19 +21,24 @@ pub use builder::SearchOptions;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-/// https://docs.atlassian.com/jira/REST/latest/
-pub struct Jira {
-    host: String,
-    username: String,
-    password: String,
+pub enum Credentials {
+    Basic(String, String),
+    // todo: OAuth
 }
 
-impl Jira {
-    pub fn new<U,P, H>(host: H, username: U, password: P) -> Jira where U: Into<String>, P: Into<String>, H: Into<String> {
+/// https://docs.atlassian.com/jira/REST/latest/
+pub struct Jira<'a> {
+    host: String,
+    credentials: Credentials,
+    client: &'a Client
+}
+
+impl<'a> Jira<'a> {
+    pub fn new<H>(host: H, credentials: Credentials, client: &'a Client) -> Jira<'a> where H: Into<String> {
         Jira {
             host: host.into(),
-            username: username.into(),
-            password: password.into()
+            credentials: credentials,
+            client: client
         }
     }
 
@@ -57,17 +62,27 @@ impl Jira {
         self.request(Method::Get, endpoint)
     }
 
+    fn authenticate(&self, method: Method, uri: &str) -> RequestBuilder {
+        let url = format!("{}/rest/api/latest{}", self.host, uri);
+        debug!("url -> {:?}", url);
+        match self.credentials {
+            Credentials::Basic(ref user, ref pass) => {
+                self.client.request(method, &url)
+                    .header(
+                        Authorization(
+                            Basic {
+                                username: user.to_owned(),
+                                password: Some(pass.to_owned())
+                            }
+                       )
+                    )
+            }
+        }
+    }
+
     fn request(&self, method: Method, endpoint: &str) -> Result<String> {
-        let cli = Client::new();
-        let url = &format!("{}/rest/api/latest{}", self.host, endpoint);
-        let req = cli.request(method, url)
-            .header(ContentType::json())
-            .header(Authorization(
-                Basic {
-                    username: self.username.clone(),
-                    password: Some(self.password.clone())
-                })
-                    );
+        let req = self.authenticate(method, endpoint)
+            .header(ContentType::json());
         let mut res = try!(req.send());
         let mut buf = String::new();
         try!(res.read_to_string(&mut buf));
