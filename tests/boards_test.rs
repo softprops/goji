e5a3cee21564e0ac9d75ca4f7fa3b29724697a0e
Daniele Esposti <daniele.esposti@gmail.com>
2018-05-20 09:41:06 +0100

    Added boards API

---
+++ tests/boards_test.rs
@@ -0,0 +1,47 @@
+extern crate goji;
+extern crate serde_json;
+
+use goji::boards::*;
+
+#[test]
+fn deserialise_board() {
+    let board_str = r#"{
+        "id": 1,
+        "self": "https://my.atlassian.net/rest/agile/1.0/board/1",
+        "name": "TEST board",
+        "type": "kanban"
+    }"#;
+
+    let board: Board = serde_json::from_str(board_str).unwrap();
+
+    assert_eq!(board.id, 1u64);
+    assert_eq!(
+        board.self_link,
+        "https://my.atlassian.net/rest/agile/1.0/board/1"
+    );
+    assert_eq!(board.name, "TEST board");
+    assert_eq!(board.type_name, "kanban");
+}
+
+#[test]
+fn deserialise_board_results() {
+    let board_results_str = r#"{
+        "maxResults": 50,
+        "startAt": 0,
+        "total": 2,
+        "isLast": true,
+        "values": [{
+            "id": 1,
+            "self": "https://my.atlassian.net/rest/agile/1.0/board/1",
+            "name": "TEST board",
+            "type": "kanban"
+        }]
+    }"#;
+
+    let board_results: BoardResults = serde_json::from_str(board_results_str).unwrap();
+
+    assert_eq!(board_results.max_results, 50u64);
+    assert_eq!(board_results.start_at, 0u64);
+    assert_eq!(board_results.is_last, true);
+    assert_eq!(board_results.values.len(), 1);
+}
