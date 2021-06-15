eb272862501e6825c60779bf4eef7f6571e0e063
Kris Scott <kscott91@gmail.com>
2021-06-15 11:19:54 -0500

    Add workaround for Jira attach. API inconsistency

--- src/attachments.rs
+++ src/attachments.rs
@@ -1,7 +1,40 @@
 //! Interfaces for accessing and managing attachments
 
+use std::collections::BTreeMap;
+
 // Ours
-use crate::{Attachment, Jira, Result};
+use crate::{Jira, Result};
+
+/// Same as `User`, but without `email_address`
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct UserResponse {
+    pub active: bool,
+    #[serde(rename = "avatarUrls")]
+    pub avatar_urls: BTreeMap<String, String>,
+    #[serde(rename = "displayName")]
+    pub display_name: String,
+    pub key: Option<String>,
+    pub name: String,
+    #[serde(rename = "self")]
+    pub self_link: String,
+    #[serde(rename = "timeZone")]
+    pub timezone: Option<String>,
+}
+
+/// Same as `Attachement`, but without `id` and with `UserResponse`
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AttachmentResponse {
+    #[serde(rename = "self")]
+    pub self_link: String,
+    pub filename: String,
+    pub author: UserResponse,
+    pub created: String,
+    pub size: u64,
+    #[serde(rename = "mimeType")]
+    pub mime_type: String,
+    pub content: String,
+    pub thumbnail: Option<String>,
+}
 
 #[derive(Debug)]
 pub struct Attachments {
@@ -17,7 +50,7 @@ impl Attachments {
     ///
     /// See this [jira docs](https://docs.atlassian.com/software/jira/docs/api/REST/8.13.8/#api/2/attachment-getAttachment)
     /// for more information
-    pub fn get<I>(&self, id: I) -> Result<Attachment>
+    pub fn get<I>(&self, id: I) -> Result<AttachmentResponse>
     where
         I: Into<String>,
     {
@@ -28,7 +61,7 @@ impl Attachments {
     ///
     /// See this [jira docs](https://docs.atlassian.com/software/jira/docs/api/REST/8.13.8/#api/2/attachment-removeAttachment)
     /// for more information
-    pub fn delete<I>(&self, id: I) -> Result<Attachment>
+    pub fn delete<I>(&self, id: I) -> Result<AttachmentResponse>
     where
         I: Into<String>,
     {
