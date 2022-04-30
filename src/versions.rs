434870aee413cf25d5a16d5d0813b9952b9e5c42
Ralf Anton Beier <ralf_beier@me.com>
2022-04-30 11:11:29 +0200

    Remove +nightly clippy warning.

--- src/versions.rs
+++ src/versions.rs
@@ -1,5 +1,5 @@
 use crate::{
-    Error, Jira, Result, Version, VersionCreationBody, VersionMoveAfterBody, VersionUpdateBody,
+    Jira, Result, Version, VersionCreationBody, VersionMoveAfterBody, VersionUpdateBody,
 };
 
 pub struct Versions {
